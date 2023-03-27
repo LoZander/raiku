cb8e563675ced8f57c7ea71a8d7dbda2a8166869
Alexander D. Troelsen <LoZander@outlook.com>
2023-03-27 23:30:13 +0200

    updated script so it removes coverage files unless its told to --keep them

--- test_coverage.sh
+++ test_coverage.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-objects="--instr-profile=raiku.profdata --object target/debug/deps/raiku-8fdcc19740f44acd --object target/debug/deps/raiku-56a01c0338693aad --object target/debug/deps/raiku-262244a53f1e9d91"
-rm *.profraw
-rm *.profdata
-RUSTFLAGS="-C instrument-coverage" cargo test --tests
-llvm-profdata merge -sparse default_*.profraw -o raiku.profdata
+clean () {
+    rm *.profraw
+    rm *.profdata
+}
 
 report () {
     llvm-cov report --use-color --ignore-filename-regex='/.cargo/registry' $objects
 }
 
-
 show () {
     llvm-cov show --use-color --ignore-filename-regex='/.cargo/registry' $objects --show-instantiations --show-line-counts-or-regions --Xdemangler=rustfilt | less -R
 }
+
+objects="--instr-profile=raiku.profdata --object target/debug/deps/raiku-8fdcc19740f44acd --object target/debug/deps/raiku-56a01c0338693aad --object target/debug/deps/raiku-262244a53f1e9d91"
+RUSTFLAGS="-C instrument-coverage" cargo test --tests
+llvm-profdata merge -sparse default_*.profraw -o raiku.profdata
 
 if [ "$1" = "report" ]; then
     report
@@ -25,4 +27,8 @@ fi
     
 if [ "$1" = "" ]; then
     report
+fi
+
+if [ "$2" != "--keep" ]; then
+    clean
 fi
